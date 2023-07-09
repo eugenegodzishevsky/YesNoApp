6ed0f3662ba9f59975bc51acb0d248024501910b
Vermut xxx <>
2023-07-09 23:11:52 +1000

    Lesson work

--- YesNoApp/YesNoApp/ViewController.swift
+++ YesNoApp/YesNoApp/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var answerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func answerButtonAction(_ sender: Any) {
+        let answer = Bool.random()
+        answerLabel.text = ""
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.answerLabel.text = answer ? "YES" : "NO"
+
+        }
+    }
 }
 
